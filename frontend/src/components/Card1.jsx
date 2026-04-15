3cc3e3c9801db15d0a83709bc0b2810a88bd568b
M-Umar-Farooq <shanisipra428@gmsil.com>
2026-04-15 16:32:28 +0500

    heroscetion setup

--- frontend/src/components/Card1.jsx
+++ frontend/src/components/Card1.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Card1 = () => {
   return (
-    <div className="absolute w-[30.3vw] h-[27.7vw] left-[29.5vw] top-[98.8vw] bg-white shadow-[0_0.4vw_0.4vw_rgba(47,97,172,0.25)] shadow-inner-[-3.7vw_-4vw_2.3vw_rgba(47,97,172,0.1)] rounded-[1vw]"></div>
+    <div 
+      className="absolute w-[20.3vw] h-[17.7vw] left-[39.5vw] top-[65.8vw] bg-white rounded-[1vw]"
+      style={{ boxShadow: "0 0.4vw 0.4vw rgba(47, 97, 172, 0.25), inset -3.7vw -4vw 2.3vw rgba(47, 97, 172, 0.1)" }}
+    >
+      <div className="absolute w-[5.6vw] h-[5.6vw]  left-[7vw] top-[2vw] bg-[#ECEBEB] rounded-full"></div>
+    </div>
   )
 }
-
 export default Card1
